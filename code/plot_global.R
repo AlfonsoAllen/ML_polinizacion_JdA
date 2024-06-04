d5ecb407d414b58382d28b578c1d54806b2e5956
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2024-06-04 09:22:08 +0200

    Update plot_global.R

--- code/plot_global.R
+++ code/plot_global.R
@@ -12,13 +12,9 @@ lines <- esp_get_can_box()
 # Plot municipalities
 
 andalucia <- esp_get_ccaa("01")
-madrid <- esp_get_ccaa("13")
-euskadi <- esp_get_ccaa("16")
 
 
 visit_rate_JdA <- read_csv("data/Features2_pred.csv") %>% dplyr::select(Longitud,Latitud, BayRidge)
-visit_rate_CAM <- read_csv("data/Features3_pred.csv") %>% dplyr::select(Longitud,Latitud, BayRidge)
-visit_rate_Eus <- read_csv("data/Features4_pred.csv") %>% dplyr::select(Longitud,Latitud, BayRidge)
 
 
 ggplot() +
@@ -27,21 +23,4 @@ ggplot() +
   scale_color_gradientn(colors = rev(wes_palette("Zissou1", type = "continuous")))+
   theme_bw()+
   labs(title = "Global model (BayRidge without MOSS)", x = "Longitude", y = "Latitude",
-       color = "log(vis.rate)")
-
-
-ggplot() +
-  geom_point(data=visit_rate_CAM, aes(x = Longitud, y = Latitud, color = BayRidge),size=2)+
-  geom_sf(data=madrid, fill=NA, linewidth = 1) +
-  scale_color_gradientn(colors = rev(wes_palette("Zissou1", type = "continuous")))+
-  theme_bw()+
-  labs(title = "Global model (BayRidge without MOSS)", x = "Longitude", y = "Latitude",
-       color = "log(vis.rate)")
-
-ggplot() +
-  geom_point(data=visit_rate_Eus, aes(x = Longitud, y = Latitud, color = BayRidge),size=2)+
-  geom_sf(data=euskadi, fill=NA, linewidth = 1) +
-  scale_color_gradientn(colors = rev(wes_palette("Zissou1", type = "continuous")))+
-  theme_bw()+
-  labs(title = "Global model (BayRidge)", x = "Longitude", y = "Latitude",
        color = "log(vis.rate)")
